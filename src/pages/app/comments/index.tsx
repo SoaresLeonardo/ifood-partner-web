2bda0c548d51f8d520688de70dd0e1b3b58a5ec4
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-05-09 11:26:33 -0300

    Init

--- src/pages/app/comments/index.tsx
+++ src/pages/app/comments/index.tsx
@@ -10,6 +10,8 @@ function Comments() {
     queryKey: ["comment", "list", "page"],
   });
 
+  console.log(comments)
+
   return (
     <div className="mx-auto max-w-4xl px-4 py-8 sm:px-6 lg:px-8">
       <div className="mb-6">
